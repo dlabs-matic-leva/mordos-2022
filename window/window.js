d992e8cac6f3fd70668cdbd2e2337fd9d8d3bc1a
matic.leva <matic.leva@dlabs.si>
2022-12-01 07:29:56 +0100

    Implement simple dragable window

---
+++ window/window.js
@@ -0,0 +1,91 @@
+customElements.define('os-window', class Files extends HTMLElement {
+    constructor() {
+        super();
+        this.mouseDown = this.mouseDown.bind(this);
+        this.mouseMove = this.mouseMove.bind(this);
+        this.mouseUp = this.mouseUp.bind(this);
+    }
+
+    connectedCallback() {
+        this.#render();
+        this.#title.addEventListener("mousedown", this.mouseDown)
+    }
+
+
+    disconnectedCallback() {
+        this.#title.removeEventListener("mousedown", this.mouseDown)
+    }
+
+    #render() {
+        this.attachShadow({mode: 'open'}).innerHTML = `
+<style>
+.window {
+    --border: var(--primary);
+    
+    position: absolute;
+    display: grid;
+    grid-auto-columns: min-content;
+    
+    background: var(--surface);
+    border: 1px solid var(--border);
+    border-radius: calc(var(--spacing) / 2);
+}
+.window-title {
+    grid-column: 1 / 2;
+    grid-row: 1 / 2;
+    
+    font-family: sans-serif;
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: var(--primary);
+    white-space: nowrap;
+    
+    margin: 0;
+    padding: var(--spacing);
+    border-bottom: 1px solid var(--border);
+    
+    cursor: pointer;
+    user-select: none;
+}
+.window-content {
+    grid-column: 1 / 2;
+    grid-row: 2 / 3;
+    
+    padding: var(--spacing);
+}
+</style>
+<div class="window">
+<div class="window-title">
+<slot name="title"></slot>
+</div>
+<div class="window-content">
+<slot></slot>
+</div>
+</div>
+`;
+    }
+
+    get #window() {
+        return this.shadowRoot.querySelector(".window");
+    }
+
+    get #title() {
+        return this.shadowRoot.querySelector(".window-title");
+    }
+
+    mouseDown() {
+        window.addEventListener("mousemove", this.mouseMove)
+        window.addEventListener("mouseup", this.mouseUp)
+        this.#window.style.transform = this.#window.style.transform || "translate(0px, 0px)"
+    }
+
+    mouseMove({movementX, movementY}) {
+        const [, x, y] = this.#window.style.transform.match(/translate\((-?\d+?)px, (-?\d+?)px\)/)
+        this.#window.style.transform = `translate(${+x + +movementX}px, ${+y + +movementY}px)`
+    }
+
+    mouseUp() {
+        window.removeEventListener("mousemove", this.mouseMove)
+        window.removeEventListener("mouseup", this.mouseUp)
+    }
+});
