6928c58d338b504b1ac99fa040826fe472bccc81
matic.leva <matic.leva@dlabs.si>
2022-12-24 23:55:20 +0100

    Improve desktop with app icons and nice background

--- window/desktop.js
+++ window/desktop.js
@@ -4,21 +4,70 @@ customElements.define('os-desktop', class extends HTMLElement {
         this.onClick = this.onClick.bind(this);
     }
 
-    #apps = ["os-camera", "os-files", "os-notes"];
+    #apps = [["os-camera", "📷"], ["os-files", "📁"], ["os-notes", "📝"]];
 
     #render() {
         this.attachShadow({mode: 'open'}).innerHTML = `
+<style>
+:host {
+    position: relative;
+    overflow: hidden;
+}
+.app-background {
+    position: absolute;
+    margin: -20px;
+    width: calc(100% + 40px);
+    height: calc(100% + 40px);
+    filter: blur(10px);
+    z-index: -1;
+}
+@media (prefers-color-scheme: dark) {
+    .app-background {
+        background: url(/window/devin-kaselnak-kEuew3XMeEA-unsplash.jpg) center / cover no-repeat;
+    }    
+
+@media (prefers-color-scheme: light) {
+    .app-background {
+        background: url(/window/martin-bennie-oJxoDn1NfZQ-unsplash.jpg) center / cover no-repeat;
+    }     
+}
+
+.app-list {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: calc(var(--spacing) * 2);
+}
+.app-icon {
+    width: calc(var(--spacing) * 10);
+    height: calc(var(--spacing) * 10);
+    border-radius: var(--spacing);
+    border: 0;
+    background: var(--shadow);
+    box-shadow: 0 0 2px var(--shadow);
+    box-sizing: border-box;
+    font-size: calc(var(--spacing) * 6);
+    line-height: calc(var(--spacing) * 10);
+    text-align: center;
+}
+</style>
+<div class="app-background"></div>
 <ul class="app-list"></ul>
 <template id="template-app-entry">
     <li class="app-entry">
-        <button class="app-name"></button>
+        <button class="app-icon"></button>
     </li>
 </template>
        `;
 
-        this.#apps.forEach(app => {
+        this.#apps.forEach(([app, icon]) => {
             const entry = this.#entryTemplate.content.cloneNode(true);
-            entry.querySelector(".app-name").textContent = app;
+            entry.querySelector(".app-icon").textContent = icon;
             entry.querySelector(".app-entry").dataset.name = app;
             this.#list.append(entry);
         })
@@ -42,7 +91,7 @@ customElements.define('os-desktop', class extends HTMLElement {
     }
 
     onClick(event) {
-        if (!event.target.classList.contains("app-name")) return;
+        if (!event.target.classList.contains("app-icon")) return;
 
         WindowManager.instance.openApp(this, event.target.parentElement.dataset.name);
     }
